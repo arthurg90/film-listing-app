09a4789ca8b9ac35bdef1ac03cac07068d93d0a8
Arthur G <agraham@WINDELL-9DU9KRP.localdomain>
2017-12-13 15:49:24 +0000

    added formatting and ratings to films list

--- src/components/FilmListItem.js
+++ src/components/FilmListItem.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const FilmListItem = ({item}) => {
+  const nextShowTime = item.showtimes[0];
+  const filmRating = item.tmdbRating;
+
   return (
-    <Text>{item.name} {item.year}</Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{item.name}</Text>
+        <Text style={styles.showtime}>{nextShowTime.startsAtDate} at {nextShowTime.startsAtTime} on {nextShowTime.channel}</Text>
+      </View>
+      { filmRating > 0 ? (
+        <View style={styles.ratingContainer}>
+        <Text style={styles.rating}> {filmRating}% </Text>
+      </View>
+    ) : null }
+    </View>
   )
 };
 
 export default FilmListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 23,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  titleContainer: {
+    flex: 1
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  showtime: {
+    fontSize: 14,
+    paddingTop: 3,
+    color: '#666'
+  },
+  ratingContainer: {
+    marginLeft: 6
+  },
+  rating: {
+    fontSize: 18
+  }
+});
